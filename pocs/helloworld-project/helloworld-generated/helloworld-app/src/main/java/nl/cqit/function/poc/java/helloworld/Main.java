658fc2782e1a20777236d3fa644ca2fa974ab3c1
CC007 <Coolcat_the_best2@Hotmail.com>
2023-04-16 23:48:11 +0200

    Add support for aot generation / native compilation

--- pocs/helloworld-project/helloworld-generated/helloworld-app/src/main/java/nl/cqit/function/poc/java/helloworld/Main.java
+++ pocs/helloworld-project/helloworld-generated/helloworld-app/src/main/java/nl/cqit/function/poc/java/helloworld/Main.java
@@ -2,9 +2,7 @@ package nl.cqit.function.poc.java.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackageClasses = {})
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
