2d6f7ad180c4b3172ee1194f797fd5a8454aadd7
Rik Schaaf <schaaf.r@tkppensioen.nl>
2023-05-05 13:36:26 +0200

    bean name update

--- pocs/boxedhello-project/boxedhello-generated/boxedhello-clients/boxedhello-helloworld-client/src/main/java/nl/cqit/function/poc/java/helloworld/controller/HelloWorldService.java
+++ pocs/boxedhello-project/boxedhello-generated/boxedhello-clients/boxedhello-helloworld-client/src/main/java/nl/cqit/function/poc/java/helloworld/controller/HelloWorldService.java
@@ -4,6 +4,6 @@ package nl.cqit.function.poc.java.helloworld.controller;
 import nl.cqit.function.poc.java.helloworld.api.HelloWorldApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "helloWorldClient", url = "http://localhost:8080")
+@FeignClient(name = "helloWorldService", url = "http://localhost:8080")
 public interface HelloWorldService extends HelloWorldApi {
 }
